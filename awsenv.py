e29a1534a3ac6b981398644b7b00d824e158e4a4
rlaidig <rlaidig@wgu.edu>
2020-12-27 13:15:04 -0700

    initial commit

---
+++ awsenv.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import fileinput
+import json
+
+def getInput():
+    arg = ""
+    for line in fileinput.input():
+        arg = arg + line
+    return arg
+
+def getCreds(creds):
+    return {
+        "AWS_ACCESS_KEY_ID": creds['AccessKeyId'],
+        "AWS_SECRET_ACCESS_KEY": creds["SecretAccessKey"],
+        "AWS_SESSION_TOKEN": creds['SessionToken']
+    }
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    arg = getInput()
+
+    creds = getCreds(json.loads(arg)['Credentials'])
+
+    for key, value in creds.items():
+        print("export {}={}".format(key,value))
